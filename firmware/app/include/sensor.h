dde9f17232cb08e27bd067bf561782abc1cecc2c
savosaicic <ssaicic@iliad-free.fr>
2026-02-25 10:15:01 +0100

    firmware: add the sensor channel abstraction type

---
+++ firmware/app/include/sensor.h
@@ -0,0 +1,23 @@
+#ifndef SENSOR_H
+
+#include <stdbool.h>
+
+#define SENSOR_MAX_CHANNELS 16
+#define SENSOR_NAME_MAX_LEN 64
+
+typedef enum {
+  SENSOR_TYPE_FLOAT = 0,
+} sensor_type_t;
+
+typedef union {
+  float f;
+} sensor_value_t;
+
+typedef struct {
+  char           name[SENSOR_NAME_MAX_LEN];
+  sensor_type_t  type;
+  sensor_value_t value;
+  bool           has_value;
+} sensor_channel_t;
+
+#endif /* SENSOR_H */
